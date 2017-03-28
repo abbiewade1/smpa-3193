8a1b56bc95cfb8bbffab6d02e78962634ecbd464
abbiewade1 <abigailw2018@gwu.edu>
2017-03-28 09:34:44 -0400

    added college scraper

--- scrape.py
+++ scrape.py
@@ -2,21 +2,33 @@ import csv
 import requests
 from BeautifulSoup import BeautifulSoup
 
-url = 'http://www.tdcj.state.tx.us/death_row/dr_scheduled_executions.html'
-response = requests.get(url)
-html = response.content
+batch_size = 2
+urllist = ["https://columbian.gwu.edu/2010-2011", "https://columbian.gwu.edu/2015-2016"]
+url_chunks = [urllist[x:x+2] for x in xrange(0, len(urllist), batch_size)]
 
-soup = BeautifulSoup(html)
-table = soup.find('table')
+def scrape_url(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    h1 = soup.find("h1", class_= "class-headline")
+    return (h1.get_text())
+
+def scrape_batch(url_chunk):
+    chunk_resp = []
+    for url in url_chunk:
+        chunk_resp.append(scrape_url(url))
+    return chunk_resp
+
+for url_chunk in url_chunks:
+    print scrape_batch(url_chunk)
 
 list_of_rows = []
-for row in table.findAll('tr')[1:5]:
+for row in table.findAll('tr')[1:-1]:
     list_of_cells = []
     for cell in row.findAll('td'):
     	list_of_cells.append(cell.text)
     list_of_rows.append(list_of_cells)
 
-outfile = open("executions", "wb")
+outfile = open("college.csv", "wb")
 writer = csv.writer(outfile)
-writer.writerow(["scheduled_execution", "link", "last_name", "first_name", "tdcj_number", "dob", "race", "date_received", "county"])
-writer.writerows(list_of_rows)
+writer.writerow(["department", "faculty", "sponsor", "title", "year"])
+writer.writerow(list_of_rows)
